1fe01b4b73f47327306053ddffeca32cca8591d5
azamjonbro <azamjondasturchi11@gmail.com>
2025-04-30 23:21:06 +0500

    sdadasdas

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
-    vue(),
-    vueDevTools(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'apple-touch-icon.png'],
